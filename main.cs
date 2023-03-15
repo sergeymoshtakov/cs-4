40ac8c9ba106afafa5b16fe1ad0f6161781371c5
sergeymoshtakov <113889721+sergeymoshtakov@users.noreply.github.com>
2023-03-15 21:22:52 +0100

    Update main.cs

--- main.cs
+++ main.cs
@@ -1,9 +1,16 @@
 using System;
 using groupNamespace;
+using studentNamespace;
 
 class Program {
   public static void Main (string[] args) {
     Group gr = new Group();
     gr.showAll();
+    Student st = gr.getStudent(ref gr);
+    Console.WriteLine(st);
+    gr.editStudent(st);
+    gr.editGroup();
+    Student st1 = new Student();
+    Console.WriteLine(st1);
   }
 }
