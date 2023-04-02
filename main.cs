601e1cf78cb4a49f202d4ce502a9660e9ffb2bb5
sergeymoshtakov <113889721+sergeymoshtakov@users.noreply.github.com>
2023-04-02 20:39:46 +0200

    Update main.cs

--- main.cs
+++ main.cs
@@ -1,16 +1,26 @@
 using System;
-using groupNamespace;
 using studentNamespace;
-using aspirantNamespace;
+using System.Collections.Generic;
 
 class Program {
   public static void Main (string[] args) {
-    Group gr = new Group();
-    Student s = new Student();
-    gr.addStudent(s);
-    Console.WriteLine(gr[0]);
-    Aspirant a = new Aspirant();
-    Console.WriteLine(a);
+    Student s1 = new Student("Ivan", "Ivanov", "Ivanovich", 2000, 5, 7, "Pushkina", 28, "Odessa", 2141, "+380-800-735-35-35", new List<int>() {12,12,12}, new List<int>() {10,9,10}, new List<int>() {10,9,10});
+    Student s2 = new Student("Ivan", "Ivanov", "Ivanovich", 2000, 5, 7, "Pushkina", 28, "Odessa", 2141, "+380-800-735-35-35", new List<int>() {12,12,12}, new List<int>() {12,12,12}, new List<int>() {10,9,10});
+    Student s3 = new Student("Ivan", "Ivanov", "Ivanovich", 2000, 5, 7, "Pushkina", 28, "Odessa", 2141, "+380-800-735-35-35", new List<int>() {12,12,12}, new List<int>() {10,3,6,2}, new List<int>() {10,9,10});
+    Student s4 = new Student("Ivan", "Ivanov", "Ivanovich", 2000, 5, 7, "Pushkina", 28, "Odessa", 2141, "+380-800-735-35-35", new List<int>() {12,12,12}, new List<int>() {10,7,2,1,10}, new List<int>() {10,9,10});
+    Student s5 = new Student("Ivan", "Ivanov", "Ivanovich", 2000, 5, 7, "Pushkina", 28, "Odessa", 2141, "+380-800-735-35-35", new List<int>() {12,12,12}, new List<int>() {8,3,5,10}, new List<int>() {10,9,10});
+    List<Student> myList = new List<Student>() {s1, s2, s3, s4, s5};
+    // myList.Sort();
+    foreach(var stud in myList)
+    {
+      Console.WriteLine(stud);
+    }
+    // Group gr = new Group();
+    // Student s = new Student();
+    // gr.addStudent(s);
+    // Console.WriteLine(gr[0]);
+    // Aspirant a = new Aspirant();
+    // Console.WriteLine(a);
     // gr.showAll();
     // Student st = gr.getStudent(ref gr);
     // Console.WriteLine(st);
